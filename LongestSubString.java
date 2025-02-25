ede7afde0d0128e4651a9bc15d95b096b40aaf25
amrit <amritkumarverma848@gmail.com>
2025-02-26 00:55:23 +0530

    Longest SubString-3

---
+++ LongestSubString.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class LongestSubString {
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> seen = new HashSet<>();
+        int max = 0;
+        int right = 0;
+        int left = 0;
+        while(right < s.length()){
+            char c = s.charAt(right);
+            if(seen.add(c)){
+                max = Math.max(max, right - left +1);
+                right++;
+            }else{
+                while(s.charAt(left) != c){
+                    seen.remove(s.charAt(left));
+                    left++;
+                }
+                seen.remove(c);
+                left++;
+            }
+        }
+        return max;
+    }
+}
